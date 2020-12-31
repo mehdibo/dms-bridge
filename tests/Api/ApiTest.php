1660351a9150927eb525c92ffe76627ec0b4b760
Mehdi Bounya <mehdi.bounya@gmail.com>
2020-12-31 11:00:01 +0100

    Improve ApiTest

--- tests/Api/ApiTest.php
+++ tests/Api/ApiTest.php
@@ -7,6 +7,7 @@ namespace Mehdibo\DmsBridge\Tests\Api;
 use League\OAuth2\Client\Provider\AbstractProvider;
 use Mehdibo\DmsBridge\Api\Api;
 use Mehdibo\DmsBridge\Entities\Account;
+use Mehdibo\DmsBridge\Entities\Transaction;
 use PHPUnit\Framework\TestCase;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Contracts\HttpClient\ResponseInterface;
@@ -63,7 +64,22 @@ class ApiTest extends TestCase
         $data = [
             'local_identifier' => 'account_id',
             'timestamp' => '2020-12-30T16:11:26.175Z',
-            'transactions' => [],
+            'transactions' => [
+                [
+                    'asset' => 10.2,
+                    'transaction_uuid' => 'uuid_1',
+                    'source' => 'src_thing',
+                    'destination' => 'dest_thing',
+                    'valid' => true,
+                ],
+                [
+                    'asset' => 18.2,
+                    'transaction_uuid' => 'uuid_2',
+                    'source' => 'src_thing_2',
+                    'destination' => 'dest_thing_1',
+                    'valid' => false,
+                ],
+            ],
             'asset' => 133.7,
         ];
         $client = $this->createClient($data);
@@ -73,6 +89,17 @@ class ApiTest extends TestCase
         $this->assertEquals($data['local_identifier'], $account->getIdentifier());
         $this->assertEquals(new \DateTime($data['timestamp']), $account->getTimestamp());
         $this->assertCount(count($data['transactions']), $account->getTransactions());
+        $i = 0;
+        foreach ($account->getTransactions() as $transaction)
+        {
+            $this->assertInstanceOf(Transaction::class, $transaction);
+            $this->assertEquals($data['transactions'][$i]['asset'], $transaction->getAsset());
+            $this->assertEquals($data['transactions'][$i]['transaction_uuid'], $transaction->getUuid());
+            $this->assertEquals($data['transactions'][$i]['source'], $transaction->getSrc());
+            $this->assertEquals($data['transactions'][$i]['destination'], $transaction->getDst());
+            $this->assertEquals($data['transactions'][$i]['valid'], $transaction->isValid());
+            $i++;
+        }
         $this->assertEquals($data['asset'], $account->getBalance());
     }
 }
